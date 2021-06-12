51b17748ee716ace1d9b23c0b4e1073d5e4d7e43
Richard L Zarth III <rlziii@icloud.com>
2021-06-12 11:12:59 -0400

    Initial Commit

---
+++ AnalyticsTest/AnalyticsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsTestApp.swift
+//  AnalyticsTest
+//
+//  Created by Richard Zarth on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
