f31985cd068ab714a35912e2588b3cba2dcb245c
Richard L Zarth III <rlziii@icloud.com>
2021-06-12 23:57:52 -0400

    Add analyticsOptions to AnalyticsView.

--- AnalyticsTest/Views/AnalyticsView.swift
+++ AnalyticsTest/Views/AnalyticsView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct AnalyticsView<Content: View, ID: Hashable>: View {
     let id: ID
+    var analyticsOptions: AnalyticsOptions = .all
     @ViewBuilder let content: Content
 
     var body: some View {
